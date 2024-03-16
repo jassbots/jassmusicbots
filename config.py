65bc4ab4adb222ec4c0c64b45e8d9a4ce2b6b491
jassbots <157136102+jassbots@users.noreply.github.com>
2024-03-16 21:14:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 # Add Owner Username without @ 
 OWNER_USERNAME = getenv("OWNER_USERNAME","Unknown_Bande")
 # Get Your bot username
-BOT_USERNAME = getenv("BOT_USERNAME" , "@MusiicYo_bot)
+BOT_USERNAME = getenv("BOT_USERNAME" , "MusiicYo_bot)
 # Don't Add style font 
 BOT_NAME = getenv("BOT_NAME" , "˹ʏᴏ ꭙ ᴍᴜsɪᴄʙᴏᴛ˼࿐")
 #get Your Assistant User name
