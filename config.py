03910c488aa9a9cf2d77ee2cf29829445d11b44c
jassbots <157136102+jassbots@users.noreply.github.com>
2024-03-16 22:02:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,12 +26,12 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", -1002139661881)
+LOGGER_ID = int(getenv("LOGGER_ID", "-1002139661881")
 
 # ------------------------------------------------
 GPT_API = getenv("GPT_API")
 # Get this value from  on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 6180225412))
+OWNER_ID = int(getenv("OWNER_ID", "6180225412"))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
