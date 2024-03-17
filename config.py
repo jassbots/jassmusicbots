bda9e83128092c6435cf7e054e5fd700299b6b41
jassbots <157136102+jassbots@users.noreply.github.com>
2024-03-17 15:46:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -41,7 +41,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/doraemon890/ANNIE-X-MUSIC",
+    "https://github.com/jassbots/jassmusicbots",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
